55a055cfe578467a2d891e4e1741906ec5667770
tonystig <tony.stig@icloud.com>
2018-11-12 10:40:03 +0800

    js-og-sdk@0.1.4 | general update .Optimize data return structure .

--- example.js
+++ example.js
@@ -4,7 +4,10 @@ var { randomBytes } = require('crypto');
 
 var og = new OG;
 
-og.setProvider(new OG.providers.HttpProvider('http://localhost:8000'))
+og.setProvider(
+    new OG.providers.HttpProvider('http://localhost:8000')
+);
+
 
 og.genesis().then(function(data){
     // console.log('genesis',data);
@@ -35,30 +38,27 @@ og.validators().then(function(data){
 });
 
 var address = '0x0b5d53f433b7e4a4f853a01e987f977497dda262';
-var hash = '0xb65308adbb94e19dc3b60c7a2a9e44b24a05c9995662e53c0b149c8600eb1dbb';
+var hash = '0xd1b2606032d8bdd217f0ba69c2c3a7c2469cc5a6cf9491b14e4705c56db7a0f4';
 
 og.getBalance(address).then(function(data){
-    console.log('getBalance',data);
+    // console.log('getBalance',data);
 });
 
 og.getNonce(address).then(function(data){
-    console.log('getNonce',data);
+    // console.log('getNonce',data);
 });
 
 og.getReceipt(hash).then(function(data){
-    console.log('getReceipt',data);
+    // console.log('getReceipt',data);
 });
 
 og.getTransaction(hash).then(function(data){
-    console.log('getTransaction',data);
+    // console.log('getTransaction',data);
 });
-// var msg = randomBytes(32);
-
-// console.log(result);
 
 var newAccount = og.newAccount();
 // console.log(newAccount);
 
 var msg = randomBytes(32);
 var result = og.signRawTransaction(msg,newAccount.privateKey);
-// console.log(result);
+console.log(result);
